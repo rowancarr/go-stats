38446adb26bce17a2a50180d902984287c08f44a
Rowan Carr <rowancarr@gmail.com>
2015-09-22 23:17:51 +0100

    Simple Stats endpoint JSON

--- main.go
+++ main.go
@@ -1,11 +1,47 @@
-// Convert markdown to html
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"syscall"
+)
 
-	"github.com/russross/blackfriday"
+//Stats ...
+type Stats struct {
+	Backups int    `json:"backups"`
+	All     uint64 `json:"all"`
+	Used    uint64 `json:"used"`
+	Free    uint64 `json:"free"`
+}
+
+//SystemStats ...
+func SystemStats(path string) (stat Stats) {
+	backupdir := os.Getenv("EP_BACKUP_DIR")
+	if backupdir == "" {
+		backupdir = "/backups"
+	}
+
+	files, _ := ioutil.ReadDir(backupdir)
+	fs := syscall.Statfs_t{}
+	err := syscall.Statfs(path, &fs)
+	if err != nil {
+		return
+	}
+	stat.All = fs.Blocks * uint64(fs.Bsize)
+	stat.Free = fs.Bfree * uint64(fs.Bsize)
+	stat.Used = stat.All - stat.Free
+	stat.Backups = len(files)
+	return
+}
+
+//const ..
+const (
+	B  = 1
+	KB = 1024 * B
+	MB = 1024 * KB
+	GB = 1024 * MB
 )
 
 func main() {
@@ -14,15 +50,19 @@ func main() {
 		port = "8080"
 	}
 
-	// Convert markdown to html
-	http.HandleFunc("/markdown", GenerateMarkdown)
-	http.Handle("/", http.FileServer(http.Dir("public")))
+	http.HandleFunc("/", ShowStats)
 	http.ListenAndServe(":"+port, nil)
 }
 
-// GenerateMarkdown ...
-func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
+//ShowStats ...
+func ShowStats(w http.ResponseWriter, r *http.Request) {
+	out := SystemStats("/")
+	js, err := json.Marshal(out)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-	markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))
-	rw.Write(markdown)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }
